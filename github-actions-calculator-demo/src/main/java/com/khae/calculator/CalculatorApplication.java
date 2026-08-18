09a433842c052316810772cb6921de8690756f60
vvekselva <vvekselva.gm@gmail.com>
2026-08-19 00:58:17 +0530

    calculator demo: add Spring Boot WAR entry point

---
+++ github-actions-calculator-demo/src/main/java/com/khae/calculator/CalculatorApplication.java
@@ -0,0 +1,19 @@
+package com.khae.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class CalculatorApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CalculatorApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculatorApplication.class, args);
+    }
+}
