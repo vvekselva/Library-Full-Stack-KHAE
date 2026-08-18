e75a97b22209ecd79147f5e365cd7a3b3beb5090
vvekselva <vvekselva.gm@gmail.com>
2026-08-18 23:48:54 +0530

    deployment: add backend application entry point

---
+++ library-deployment-activity/backend/src/main/java/com/khae/librarydeployment/LibraryDeploymentApplication.java
@@ -0,0 +1,11 @@
+package com.khae.librarydeployment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryDeploymentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryDeploymentApplication.class, args);
+    }
+}
