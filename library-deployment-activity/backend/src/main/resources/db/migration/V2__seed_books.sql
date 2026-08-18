92ff39f6b485ad64f3b14a1fe2d58385703973fe
vvekselva <vvekselva.gm@gmail.com>
2026-08-18 23:49:20 +0530

    deployment: add Flyway seed data

---
+++ library-deployment-activity/backend/src/main/resources/db/migration/V2__seed_books.sql
@@ -0,0 +1,5 @@
+INSERT INTO books (title, author, isbn, available_copies)
+VALUES
+    ('Clean Code', 'Robert C. Martin', '9780132350884', 4),
+    ('Effective Java', 'Joshua Bloch', '9780134685991', 3),
+    ('Spring in Action', 'Craig Walls', '9781617297571', 5);
